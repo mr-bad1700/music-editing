865d27fc299546a8c312b1f79b8a4cad4b57e010
Mr.BAD <79611127+mr-bad1700@users.noreply.github.com>
2021-05-11 16:41:41 +0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -89,9 +89,9 @@ module.exports = {
         if (serverQueue) {
             serverQueue.songs.push(song);
             let thing = new MessageEmbed()
-                .setAuthor("Song has been added to queue", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+                .setAuthor(client.user.username, client.user.avatarURL())
                 .setThumbnail(song.img)
-                .setColor("YELLOW")
+                .setColor("#303135")
                 .addField("Name", song.title, true)
                 .addField("Duration", song.duration, true)
                 .addField("Requested by", song.req.tag, true)
@@ -163,9 +163,9 @@ module.exports = {
 
             dispatcher.setVolumeLogarithmic(queue.volume / 100);
             let thing = new MessageEmbed()
-                .setAuthor("Started Playing Music!", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+                .setAuthor(client.user.username, client.user.avatarURL())
                 .setThumbnail(song.img)
-                .setColor("BLUE")
+                .setColor("#303135")
                 .addField("Name", song.title, true)
                 .addField("Duration", song.duration, true)
                 .addField("Requested by", song.req.tag, true)
